dd4bd32cc924307860f069ec854d1931ec1baca7
raouf <raoufsirat18@gmail.com>
2021-05-22 15:28:07 +0430

    component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {UserComponent } from './user/user.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
